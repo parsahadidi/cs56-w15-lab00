cb126cd0cf72ba09b8c4d208db7966ccfaec133b
Parsa Hadidi <parsahadidi@umail.ucsb.edu>
2015-01-08 18:41:07 -0800

    Added Javadoc Comment

---
+++ MyFirstApp.java
@@ -0,0 +1,15 @@
+/** MyFirstApp is a sample program from p. 8-9 of Head First Java, 2nd edition.
+ @author Parsa Hadidi & Ryan Peffers
+ @version 01/08/2015 for lab00, cs56, W15
+*/
+
+
+public class MyFirstApp {
+
+    public static void main (String[] args) {
+	System.out.println("I Rule!");
+	System.out.println("The World");
+
+    }
+
+}
